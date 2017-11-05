02a70345b191ccd1c634e2607b9862c07c91fbf6
anka-kmbl <anna.maksymenko@nure.ua>
2017-11-05 13:16:19 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  toggle(dimension: string) {
+    for(let key in this.hideDivs) {
+      this.hideDivs[key] = true;
+    }
+    console.log(this.hideDivs);
+    this.hideDivs[dimension] = false;
+    console.log(this.hideDivs);
+  }
+
+  hideDivs: any = {
+    cooking: false,
+    shoes: true
+  }
 }
